75c2b794fd07957bdb717cfcbb6ae6590d0623c7
ADITYA MISHRA <c-aditya.mishra@timesinternet.in>
2022-09-07 16:06:45 +0530

    added test case

---
+++ myFirstPodsSample/myFirstPodsSampleAppTests/TimesStampsTests.swift
@@ -0,0 +1,29 @@
+//
+//  TimesStampsTests.swift
+//  myFirstPodsSampleAppTests
+//
+//  Created by Aditya Mishra on 07/09/22.
+//
+
+import XCTest
+
+@testable import myFirstPodsSample
+
+class TimesStampsTests: XCTestCase {
+    
+    var myTimesStamp:  TimeStamps!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        myTimesStamp = TimeStamps()
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        myTimesStamp = nil
+    }
+    
+    func testTimeStamp() {
+        XCTAssertEqual(myTimesStamp.setTimestamp(epochTime: "0"), "6 days ago")
+    }
+}
