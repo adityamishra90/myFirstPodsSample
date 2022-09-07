7bb3a4c3fd8bd54746e478d8c203aeb4c98c7f57
ADITYA MISHRA <c-aditya.mishra@timesinternet.in>
2022-09-07 15:00:14 +0530

    added project framework

---
+++ myFirstPodsSample/myFirstPodsSample/myFirstPodsSample.h
@@ -0,0 +1,18 @@
+//
+//  myFirstPodsSample.h
+//  myFirstPodsSample
+//
+//  Created by Aditya Mishra on 07/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for myFirstPodsSample.
+FOUNDATION_EXPORT double myFirstPodsSampleVersionNumber;
+
+//! Project version string for myFirstPodsSample.
+FOUNDATION_EXPORT const unsigned char myFirstPodsSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myFirstPodsSample/PublicHeader.h>
+
+
